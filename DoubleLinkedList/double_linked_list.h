feec928d08f34c2f4e1a1875ff9d6a17fa1da087
VVLtowl <86180375+VVLtowl@users.noreply.github.com>
2022-10-07 18:07:56 +0900

    update file

--- DoubleLinkedList/double_linked_list.h
+++ DoubleLinkedList/double_linked_list.h
@@ -1,86 +1,83 @@
 #pragma once
+#include "ScoreData.h"
 
-const int WORD_SIZE = 50;/**<文字列読み取の上限*/
-
+//========== 双方向リスト ==========
 class DoubleLinkedList
 {
 public:
-	/**成績データ構造体*/
-	struct ScoreData
-	{
-		int score;				/**<スコア*/
-		char name[WORD_SIZE];	/**<ユーザー名*/
-
-		/**コンストラクタでメンバー変数を初期設定*/
-		//ScoreData(int _score, string _kName) :
-		//	score(_score),
-		//	kName(_kName) {};
-	};
-
-	/**ノード構造体*/
+	//========== ノード構造体 ==========
 	struct Node
 	{
-		Node* prev = nullptr;			/**<目のノードへのポインタ*/
-		Node* next = nullptr;			/**<次のノードへのポインタ*/
-		ScoreData scoreData;			/**<成績データ*/
+		Node* pPrev = nullptr;			//目のノードへのポインタ
+		Node* pNext = nullptr;			//次のノードへのポインタ
+		ScoreData scoreData;			//成績データ
+		bool isDummy;					//ダミーノード判断用
+
+		Node();								//コンストラクタ　ダミーノードとして呼び出した時
+		Node(int score, const char* name);	//コンストラクタ　要素ノードとして呼び出した時
 	};
 
-	/** コンストイテレータ*/
+	//==========  コンストイテレータ ==========
 	class ConstIterator
 	{
-	private:
-		Node* node;												/**< Node型のイテレータ*/
-		Node n;
-	public:
-		ConstIterator() { node = new Node(); };
-		void CopyNode(Node* node);								/**<ノードポインタを設定*/
-		Node* GetNode() { return node; };						/**<ノードポインタを取得*/
+	protected:
+		Node* m_pNode;												// Node要素へのポインタ
+		const DoubleLinkedList* m_pList;							//リストへのポインタ
+		bool IsVaild(const DoubleLinkedList* const list);			//有効なイテレータであるか
 
-		ConstIterator operator--();								/**<リストの先頭に向かって一つ進める*/
-		ConstIterator operator++();								/**<リストの末尾に向かって一つ進める*/
-		const ConstIterator operator*();						/**<イテレータの指す要素を取得する(const版)*/
-		ConstIterator(const ConstIterator& constIterator);		/**<コピーを行う（コピーコンストラクタ）*/
-		ConstIterator& operator=(ConstIterator& constIterator);	/**<代入を行う*/
-		bool operator==(ConstIterator& constIterator);			/**<同一か比較する*/
-		bool operator!=(ConstIterator& constIterator);			/**<異なるか比較する*/
+	public:
+		//ConstIterator();     //コンストラクタ　
+		ConstIterator(Node* node,const DoubleLinkedList* list) :
+			m_pNode(node), m_pList(list) { };						//コンストラクタで参照を設定
+	
+		ConstIterator& operator--();								//リストの先頭に向かって一つ進める
+		ConstIterator& operator++();								//リストの末尾に向かって一つ進める
+		ConstIterator operator--(int i);								//リストの先頭に向かって一つ進める
+		ConstIterator operator++(int i);								//リストの末尾に向かって一つ進める
+		Node& operator*()const;										//イテレータの指す要素を取得する(const版)
+		Node* operator&()const;										//指す要素へのポインタを取得する(const版)
+		ConstIterator(const ConstIterator& constIter);				//コピーを行う（コピーコンストラクタ）
+		ConstIterator& operator=(const ConstIterator& constIter);	//代入を行う
+		bool operator==(const ConstIterator& constIter);			//同一か比較する
+		bool operator!=(const ConstIterator& constIter);			//異なるか比較する
+
+
+		friend DoubleLinkedList;									//DoubleLinkedListの関数内で、有効イテレータ確認が行えるため
 	};
 
-	/** イテレータ*/
+	//========== イテレータ ==========
 	class Iterator :public ConstIterator
 	{
 	public:
-		Iterator operator*();		/**<イテレータのさす要素を取得する(非const版)*/
+		//Iterator() {};
+		Iterator(Node* node,const DoubleLinkedList* list) :
+			ConstIterator(node, list) {};						//コンストラクタで参照を設定
+
+		Node& operator*();										//イテレータのさす要素を取得する(非const版)
+		Node* operator&();										//指す要素へのポインタを取得する(非const版)
 	};
 
 private:
-	Node* head;							/**<先頭ノードへのポインタ*/
-	Node* tail;							/**<末尾ノードへのポインタ*/
-	Iterator* iterator;					/**<ノードのイテレータ*/
-	ConstIterator* constIterator;		/**<ノードのコンストイテレータ*/
+	Node* m_pDummy;										//ノードへのポインタ 
+	Node* HeadNode() const { return m_pDummy->pNext; };	//先頭ノードへのポインタを取得
 
 public:
-	~DoubleLinkedList(){
-		delete iterator;
-		delete constIterator;
+	~DoubleLinkedList(){								//デストラクタ定義
+		delete m_pDummy;
 	}
-	DoubleLinkedList(){
-		head = nullptr;
-		tail = nullptr;
-		iterator = new Iterator();
-		constIterator = new ConstIterator();
+	DoubleLinkedList(){									//コンストラクタ定義
+		m_pDummy = new Node();							
+		m_pDummy->isDummy = true;
 	}
 
-	Node* GetHead() { return head; };
-	Node* GetTail() { return tail; };
-
-	void PushBack(Node* newNode);		/**<データを末尾後に追加*/
-
-	int Size();							/**<データ数を取得する*/
-	void Add(Iterator iterator);		/**<データの挿入*/
-	void Remove(Iterator iterator);		/**<データの削除*/
-	Iterator Head();					/**<先頭イテレータ取得する*/
-	ConstIterator ConstHead();			/**<先頭コンストイテレータ取得*/
-	Iterator Tail();					/**<末尾イテレータ取得する*/
-	ConstIterator ConstTail();			/**<末尾コンストイテレータ取得*/
+	int Count()const;									//データ数を取得する
+	bool PushBack(Node* newNode);						//データを末尾後に追加
+	bool PushBack(int score,const char* name);			//データを末尾後に追加
+	bool Insert(ConstIterator& position, Node* newNode);	//データの挿入
+	bool Remove(ConstIterator& position);				//データの削除
+	Iterator Begin();									//先頭イテレータ取得する
+	ConstIterator CBegin()const;						//先頭コンストイテレータ取得
+	Iterator End();										//末尾イテレータ取得する
+	ConstIterator CEnd()const;							//末尾コンストイテレータ取得
 };
 
